a9fe1314f4c81eec80dd1ef4cac5fbd192b8c6cb
Rikimaula <145535564+Rikimaula@users.noreply.github.com>
2024-08-28 10:42:37 +0700

    Add files via upload

---
+++ bab13soal1.java/src/main/java/com/mycompany/bab13soal1/java/Bab13soal1Java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab13soal1.java;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Bab13soal1Java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ bab9soal3.java/src/main/java/com/mycompany/bab9soal3/java/Bab9soal3Java.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab9soal3.java;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Bab9soal3Java {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i < 6) {
+          System.out.println(i);
+          i++;
+    }
+    }
+}
